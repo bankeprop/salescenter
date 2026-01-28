961827a4a7585431d53af238d126625a434df630
kavinda-banke <kavinda@banke.ae>
2026-01-28 10:47:53 +0400

    Damac lagoon

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import EmaarHeight from './Pages/Emaar/EmaarHeight';
 import MercedesBenzPlaces from './Pages/Binghati/MercedesBenzPlaces';
 import MercedesThankYou from './Pages/Binghati/MercedesThankYou';
 import YasCanel from './Pages/Ohana/YasCanel';
+import DamacLagoonValencia from './Pages/Damac/DamacLagoonValencia';
+import DamacLagoonValenciaThanks from './Pages/Damac/DamacLagoonValenciaThanks';
 
 function App() {
   return (
@@ -16,11 +18,16 @@ function App() {
         {/* <Route path="/" element={<Navigate to="/DamacIsland" />} /> */}
         <Route path="/Damac/DamacIsland" element={<DamacIsland />} />
         <Route path="/Damac/DamacIslandThanks" element={<DamacIslandsThanks />} />
+        <Route path="/Damac/DamacLagoonValencia" element={<DamacLagoonValencia />} />
+        <Route path="/Damac/DamacLagoonValenciaThanks" element={<DamacLagoonValenciaThanks />} />
+
         <Route path="/Emaar/EmaarTheValley" element={<EmaarValley />} />
         <Route path="/EmaarvalleyThanks" element={<EmaarvalleyThanks />} />
         <Route path="/Emaar/EmaarHeights" element={<EmaarHeight />} />
+
         <Route path="/Binghatti/MercedesBenzPlaces" element={<MercedesBenzPlaces />} />
         <Route path="/Binghatti/ThankYou" element={<MercedesThankYou />} />
+
         <Route path="/Ohana/YasCanel" element={<YasCanel />} />
 
       </Routes>
